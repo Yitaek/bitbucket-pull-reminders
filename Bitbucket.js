0bd01dc3b537c21da4e08d53a347a473222f6b59
Yitaek Hwang <yitaek@gmail.com>
2020-01-21 23:19:27 -0500

    Add GCP KMS functionality (#1)
    * Adding KMS scripts
    
    * Refactor bitbucket to its own class
    
    * adding linter
    
    * Refactoring slack
    
    * Add kms functionality

---
+++ Bitbucket.js
@@ -0,0 +1,99 @@
+const BitbucketAPI = require('bitbucket')
+
+class Bitbucket {
+  constructor( options ) {
+    this.bitbucket = new BitbucketAPI()
+    this.bitbucket.authenticate(options.auth)
+    this.teamName = options.teamName
+  }
+
+  async getAllPRs() {
+    try {
+      const slugs = await this.getAllRepos(1)
+      const pullRequests = await this.getPRs(slugs)
+      const reviewers = await this.getReviewers(pullRequests)
+
+      return reviewers
+    } catch (err) {
+      console.log(err)
+      return null
+    }
+  }
+
+  async getAllRepos(pageNumber) {
+    const {data} = await this.bitbucket.repositories.list({
+      username: this.teamName,
+      pagelen: 100,
+      page: pageNumber,
+      q: '(project.key="LP" OR project.key="JUMP")' // OR project.key="JUMP" OR project.key="MAR" OR project.key="VEIC" OR project.key="SM")'  TODO - make this configurable ...
+    })
+
+    const {values, page, next} = data
+    let slugs = []
+
+    if (next) {
+      slugs = await this.getAllRepos(page + 1)
+    }
+
+    const repoSlugs = values.map( (repo) => {
+      return repo.slug
+    })
+
+    return slugs.concat(repoSlugs)
+  }
+
+  async getPRs(slugs) {
+    const pullRequests = []
+    for (let i = 0; i < slugs.length; i++) {
+      const {data} = await this.bitbucket.repositories.listPullRequests({
+        repo_slug: slugs[i],
+        username: this.teamName
+      })
+
+      if (data.size > 0) {
+        const slug = slugs[i]
+        const ids = data.values.map((pr) => {
+          return pr.id
+        })
+        const idsBySlug = {
+          slug,
+          ids
+        }
+        pullRequests.push(idsBySlug)
+      }
+    }
+
+    return pullRequests
+  }
+
+  async getReviewers(pullrequest) {
+    const reviewers = []
+    for (let i=0; i < pullrequest.length; i++) {
+      const ids = pullrequest[i].ids
+      const slug = pullrequest[i].slug
+      for (let j=0; j <ids.length; j++) {
+        const {data} = await this.bitbucket.repositories.getPullRequest({
+          username: this.teamName,
+          repo_slug: slug,
+          pull_request_id: ids[j]
+        })
+
+        const {title, participants, updated_on, links} = data
+        const waitingReview = participants.filter( (reviewer) => !reviewer.approved)
+        const reviewerName = waitingReview.map( (review) => review.user.display_name)
+        const link = links.html.href
+
+        reviewers.push({
+          title,
+          updated_on,
+          reviewerName,
+          link
+        })
+      }
+    }
+    // loop thru slug and ids and grab revewer
+    return reviewers
+  }
+}
+
+module.exports = Bitbucket
--- index.js
+++ index.js
@@ -1,131 +1,20 @@
-const Bitbucket = require('bitbucket')
-const { IncomingWebhook } = require('@slack/webhook')
-const parseISO = require('date-fns/parseISO')
-const differenceInDays = require('date-fns/differenceInDays')
-const config = require('./Config')
+const Config = require('./Config')
+const Bitbucket = require('./Bitbucket')
+const Slack = require('./Slack')
 
-const bitbucket = new Bitbucket()
-const teamName = process.env.TEAMNAME
+// exports.sendPullRequestReminders = async (event, context) => {
+//   await getAllPRs()
+// };
 
-bitbucket.authenticate(config.bitbucket)
 
-exports.sendPullRequestReminders = async (event, context) => {
-  await getAllPRs(teamName)
-};
+getAllPRs()
 
-async function getAllPRs(teamName) {
-  try {
-    const slugs = await getAllRepos(1)
-
-    const pullRequests = await getPRs(slugs, bitbucket)
-    const reviewers = await getReviewers(pullRequests)
-
-    await sendToSlack(reviewers)
-    
-  } catch(err){
-    console.log(err)
-  } 
-}
-
-async function getAllRepos(pageNumber){
-  const { data } = await bitbucket.repositories.list({ 
-    username: teamName,
-    pagelen: 100,
-    page: pageNumber,
-    q: '(project.key="LP" OR project.key="JUMP" OR project.key="MAR" OR project.key="VEIC" OR project.key="SM")' // TODO - make this configurable ... 
-  })
-
-  const { values, size, page, next } = data
-  let slugs = []
+async function getAllPRs() {
+  const config = await Config.create()
   
-  if(next){
-    slugs = await getAllRepos(page + 1)
-  }
+  const bitbucket = new Bitbucket( config.bitbucket )
+  const slack = new Slack( config.slack )
 
-  const repoSlugs = values.map( repo => {
-    return repo.slug
-  })
-
-  return slugs.concat(repoSlugs)
-}
-
-async function sendToSlack(message){
-
-  const DATE_TODAY = Date.now()
-
-  const blocks = message.map( pr => {
-    const reviewers = pr.reviewerName.join(', ')
-    const prTime = parseISO(pr.updated_on)
-    const timeDiff = differenceInDays(DATE_TODAY, prTime)
-    const link = pr.link
-    const slackMsg = {
-      "type": "section",
-      "text": {
-        "type": "mrkdwn",
-        "text": `<${link}|${pr.title}> - ${timeDiff} days old - Waiting on ${reviewers}`
-      }
-    }
-
-    return slackMsg
-  })
-
-  const url = process.env.SLACK_WEBHOOK_URL
-  const webhook = new IncomingWebhook(url)
-
-  await webhook.send({
-    text: "Pull request reminders", 
-    blocks
-  })
-
-}
-
-async function getReviewers(pullrequest) {
-  const reviewers = []
-  for (let i=0; i < pullrequest.length; i++){
-    const ids = pullrequest[i].ids
-    const slug = pullrequest[i].slug
-    for (let j=0; j <ids.length; j++){
-      const { data } = await bitbucket.repositories.getPullRequest({ 
-        username: teamName, 
-        repo_slug: slug,
-        pull_request_id: ids[j]
-      })
-
-      const { title, participants, updated_on, links } = data
-      const waitingReview = participants.filter( reviewer => !reviewer.approved) 
-      const reviewerName = waitingReview.map( review => review.user.display_name)
-      const link = links.html.href
-
-      reviewers.push({
-        title,
-        updated_on,
-        reviewerName,
-        link
-      })
-    }
-  }
-  // loop thru slug and ids and grab revewer
-  return reviewers
-}
-
-async function getPRs(slugs, bitbucket) {
-  const pullRequests = []
-  for (let i = 0; i < slugs.length; i++) {
-    const { data } = await bitbucket.repositories.listPullRequests({
-      repo_slug: slugs[i],
-      username: teamName
-    })
-
-    if(data.size > 0){
-      const slug = slugs[i]
-      const ids = data.values.map(pr => { return pr.id })
-      const idsBySlug = { 
-        slug,
-        ids
-      }
-      pullRequests.push(idsBySlug)
-    }
-  }
-
-  return pullRequests
+  const reviewers = await bitbucket.getAllPRs()
+  await slack.sendToSlack(reviewers)
 }
